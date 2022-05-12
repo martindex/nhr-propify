b24bd5e368bdd0eb7307b80e255c090cf000c49a
Martin Tapia <mtapia@palmatools.com>
2022-05-12 01:22:40 -0300

    done tables

--- src/main/resources/db/migration/V1__baseline.sql
+++ src/main/resources/db/migration/V1__baseline.sql
@@ -8,3 +8,20 @@ CREATE TABLE property (
   code VARCHAR(10) NOT NULL
 -- TODO: Add missing columns, indices, constraints
 );
+
+CREATE TABLE address (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  street VARCHAR(100) NOT NULL,
+  city VARCHAR(100) NOT NULL,
+  state VARCHAR(2) NOT NULL,
+  zip VARCHAR(5) NOT NULL,
+  timezone VARCHAR(100)
+);
+
+CREATE TABLE property_type (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  type VARCHAR(10) NOT NULL
+);
+
+-- TODO constraints and indices will be add when endpoint is run
+

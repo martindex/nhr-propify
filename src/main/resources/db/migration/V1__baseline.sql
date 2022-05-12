56ed34ccd4476144f0abe390d0d20940035c9e04
Martin Tapia <mtapia@palmatools.com>
2022-05-12 01:09:33 -0300

    added columns in baseline

--- src/main/resources/db/migration/V1__baseline.sql
+++ src/main/resources/db/migration/V1__baseline.sql
@@ -1,5 +1,10 @@
 CREATE TABLE property (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  rent_price FLOAT NOT NULL
+  rent_price FLOAT NOT NULL,
+  create_time VARCHAR(50),
+  property_type_id INT,
+  address_id INT NOT NULL,
+  email VARCHAR(100),
+  code VARCHAR(10) NOT NULL
 -- TODO: Add missing columns, indices, constraints
 );
